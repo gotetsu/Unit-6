9c0a302d1f9890e80591fa608a31c8b8c095b963
Nicolas <nicosupremetech@gmail.com>
2021-04-08 19:50:29 +1200

    Fnished
    Fnished

--- js/app.js
+++ js/app.js
@@ -1,39 +1,124 @@
-// const keyboard = document.getElementById('qwerty');
-// const phrase = document.querySelector('#phrase');
-// const resetButton = document.getElementById('start-game');
-// const overlay = document.getElementById('overlay');
-// const scoreboard = document.getElementById('scoreboard');
-// const hearts = document.querySelectorAll('li.tries > img')
-// let missed = 0;
-
-const qwerty = document.getElementById('qwerty');
-const phrase = document.getElementById('phrase'); 
-const resetbtn =  document.getElementsByClassName('btn__reset');
+const keyboard = document.getElementById('qwerty');
+const phrase = document.querySelector('#phrase');
+const resetButton = document.getElementById('startG');
+const overlay = document.getElementById('overlay');
+const scoreboard = document.getElementById('scoreboard');
+const hearts = document.querySelectorAll('li.tries > img')
 let missed = 0;
+
 const phrases = [
-    'Hola mi nombre es Juan',
-    'I have not been in love',
-    'Tell me something good',
-    'Dime algo bueno',
-    'Walk the plank',
-    'el conquistador',
-    'Tell me everything you know',
-    'Caliente',
-    'My Arms are Warm now ',
-    'Tomate un cafecito',
-    'yes you are not the person you want to be',
-    'Scare or scary',
-    'son of a gun',
-    'Si means Yes in Spanish',
-    'Santa is coming to town'
-  ]
 
-  resetbtn.addEventListener('click', () => {
-    overlay.style.display = 'none';
+'yes can I Help you ',
+'My name is Roger',
+'No way Jose',
+'This is Atis Nice to meet',
+'Hello World',
+'Do not open the door',
+'yup this is my victory'
+
+
+]
+
+resetButton.addEventListener('click', () => {
+  overlay.style.display = 'none';
+  setUpGame();
+});
+
+function getRandomPhraseAsArray(allPhrases) {
+  randomIndex = Math.floor(Math.random() * allPhrases.length);
+  phraseToGuess = allPhrases[randomIndex];
+  return phraseToGuess.split('')
+}
+
+function addPhraseToDisplay(phraseArr) {
+  phraseArr.forEach(letter => {
+    const li = document.createElement("li");
+    const phraseUl = document.querySelector('#phrase > ul');
+    phraseUl.appendChild(li);
+    if(letter !== ' ') {
+      li.setAttribute("class", "letter")
+    } else {
+      li.setAttribute("class", "space")
+    }
+    li.textContent = letter;
   });
+}
 
-  function getRandomPhraseAsArray(allPhrases) {
-    randomIndex = Math.floor(Math.random() * allPhrases.length);    // this function will get a getRandom Phrase
-    phraseToGuess = allPhrases[randomIndex];
-    return phraseToGuess.split('')
+
+function checkLetter(pressedKey) {
+  let selectedLetter = null
+  const allLetters = document.querySelectorAll('#phrase > ul > li.letter')
+  allLetters.forEach(letter => {
+    if(letter.innerHTML.toLowerCase() === pressedKey) {
+      letter.classList.add("show")
+      selectedLetter = pressedKey
+    }
+  })
+  return selectedLetter
+}
+
+
+keyboard.addEventListener('click', (e) => {
+  let pressedKey = (e.target.innerHTML).toLowerCase();
+  if (e.target.tagName === 'BUTTON') {
+    e.target.setAttribute("class", "chosen");
+    e.target.setAttribute("disabled", "true");
+    let letterFound = checkLetter(pressedKey);
+    if(!letterFound) {
+      missed += 1;
+      for (let i = 0; i < missed; i++) {
+        hearts[i].setAttribute("src", "images/lostheart.png")
+      }
+    }
+    if (checkWin()) {
+      updateOverlay("win", 'You won!');
+    } else if (missed >= 5) {
+      updateOverlay("lose", 'You lost!');
+    }
   }
+})
+
+function checkWin() {
+  const allLetters = document.querySelectorAll('#phrase > ul > li.letter')
+  const displayedLetters = document.querySelectorAll('#phrase > ul > li.show')
+  return allLetters.length === displayedLetters.length
+}
+function setUpGame() {
+  
+    missed = 0;
+ 
+    const ul = document.createElement("ul");
+    const phraseUl = document.querySelector('#phrase > ul');
+    phrase.replaceChild(ul, phraseUl);
+   
+    const phraseArray = getRandomPhraseAsArray(phrases);
+    addPhraseToDisplay(phraseArray);
+  
+    const keys = document.querySelectorAll('.keyrow > button.chosen');
+    keys.forEach(key => {
+      key.removeAttribute("class", "chosen");
+      key.removeAttribute("disabled", "true");
+    });
+    
+    hearts.forEach(heart => {
+      heart.setAttribute("src", "images/liveheart.png")
+    });
+    
+    const message = document.querySelector('#overlay > p.message');
+    if (message) {
+      message.remove();
+    };
+  }
+
+function updateOverlay(status, message) {
+  overlay.style.display = 'flex';
+  overlay.setAttribute("class", status);
+  const p = document.createElement("p");
+  p.setAttribute("class", "message");
+  p.style.backgroundColor = 'inherit';
+  p.style.fontSize = 'xx-large';
+  overlay.appendChild(p);
+  let text = message;
+  p.textContent = text;
+  resetButton.textContent = "Play again";
+}
